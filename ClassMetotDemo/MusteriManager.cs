54560d2ee9f284a65e9680f8b81f99e9857d84ff
RIDVAN BOZKIR <ridvanbozkir@yandex.com>
2021-01-31 14:38:00 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Yeni bir müşteri eklendi. Müşteri Adı:" +musteri.Ad +" " + musteri.HesapNo);
+        }
+
+        public void MusteriDuzenle(Musteri musteri1)
+        {
+            Console.WriteLine("Müşteri bilgileri düzenlendi Yeni müşteri Adı: " + musteri1.Ad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri = new Musteri();
+
+            musteri.Ad = "Hasan";
+            musteri.Soyad = "Ali";
+            musteri.Adres = "ANKARA";
+            musteri.HesapNo = "TR12 2021 2154 6587 7849 21";
+            musteri.Bakiye = 2542365206.54;
+
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Ad = "Osman Kale";
+
+            MusteriManager musteriManager = new MusteriManager();
+            
+            musteriManager.MusteriEkle(musteri1);
+        }
+    }
+}
